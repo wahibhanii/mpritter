e239708724d658135114349bdb3cbd7cfb4146bd
wahibhanii <wahib.hanii@gmail.com>
2018-01-18 15:02:51 +0700

    checkpint 3

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -8,7 +8,34 @@ const jwt         = require('jsonwebtoken');
 class PostsController {
 
   static createPost (req, res) {
+    console.log(req.body)
+    let newPost = {
+      content : req.body.content,
+      createdAt: new Date(),
+      hashtags : [],
+      poster: null,// request.headers.decoded._id
+    }
     
+    let arrTag = newPost.content.split(' ')
+    console.log(arrTag)
+    arrTag = arrTag.filter(word => {
+      console.log(word[0] === '#')
+      return (word[0] === '#')
+    })
+    console.log('>>>', arrTag)
+    newPost.hashtags = arrTag
+
+    Post.create(newPost)
+    .then(result => {
+      res.status(200).json({
+        message: 'Post Created',
+        data: result
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send(err)
+    })
   }
 
   static getAllPosts(req, res) {
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,1 +1,24 @@
 
+'use strict'
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    content: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return v.length <= 140
+        },
+        message : 'Pritt cannot be more than 140 Character'
+      },
+      required: [true, 'Pritt is required']
+    },
+    hashtags: [String],
+    createdAt: Date,
+    poster: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
